416a6c4a4ecb2ef57511fb155c397a095c92a5a7
Jouskaio <jouskaio.me@gmail.com>
2021-09-08 20:14:34 +0200

    Launch Screen

---
+++ Randomizer/Controllers/RandomizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomizerApp.swift
+//  Randomizer
+//
+//  Created by Saturne on 08/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct RandomizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Randomizer/Views/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Randomizer
+//
+//  Created by Saturne on 08/09/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("by Jouskaio")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
